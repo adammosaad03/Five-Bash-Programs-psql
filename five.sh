7895f57bf2b19e85ab9a3cac63a7e416f604f52e
adammosaad03 <mosaadadam647@gmail.com>
2024-12-14 22:20:29 -0500

    Create five.sh

---
+++ five.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Program to run my other four programs
+
+
+
+echo -e "\n~~ Running All Programs ~~\n"
+
+# Add commands to run your programs here
+
+echo "Running questionnaire.sh..."
+./questionnaire.sh
+./countdown.sh 3
+./bingo.sh
+./fortune.sh
+
+echo -e "\n~~ All Programs Executed ~~\n"
